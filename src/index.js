2201b479e9e24d828625fda6feb53bae2375e695
Rainspistols <ffly@mail.ru>
2020-02-23 13:51:20 +0100

    created api client

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
-const getResource = async url => {
-  const res = await fetch(url);
-  const body = await res.json();
-  return body;
-};
+class SwapiService {
+  _apiBase = 'https://swapi.co/api';
 
-getResource('https://swapi.co/api/people/1/')
+  async getResource(url) {
+    const res = await fetch(this._apiBase + url);
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, recieved ${res.status}`);
+    }
+    return await res.json();
+  }
+
+  Get = {
+    person: async id => {
+      return this.getResource(`/people/${id}`);
+    },
+    starship: async id => {
+      return this.getResource(`/starships/${id}`);
+    },
+    planet: async id => {
+      return this.getResource(`/planets/${id}`);
+    },
+    All: {
+      people: async () => {
+        const response = await this.getResource('/people/');
+        return response.results;
+      },
+      planets: async () => {
+        const response = await this.getResource('/planets/');
+        return response.results;
+      },
+      starships: async () => {
+        const response = await this.getResource('/starships/');
+        return response.results;
+      }
+    }
+  };
+}
+
+const swapi = new SwapiService();
+
+swapi.Get.All.starships().then(people => {
+  people.forEach(person => console.log(person.name));
+});
