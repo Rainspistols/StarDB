90b586bc95b4fe347233aed04ff8686cab724f65
Rainspistols <ffly@mail.ru>
2020-03-15 14:04:21 +0100

    Updates

---
+++ precache-manifest.b7e71423144e69e491552585b865bbce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea649390f8547747e3ea5b458d3f504",
+    "url": "/StarDB/index.html"
+  },
+  {
+    "revision": "715e480fb2bb67f138ac",
+    "url": "/StarDB/static/css/main.b0610215.chunk.css"
+  },
+  {
+    "revision": "5820e64f287c095d50dd",
+    "url": "/StarDB/static/js/2.dce983d6.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/StarDB/static/js/2.dce983d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "715e480fb2bb67f138ac",
+    "url": "/StarDB/static/js/main.094fc00a.chunk.js"
+  },
+  {
+    "revision": "4ed432024c7deca5470d",
+    "url": "/StarDB/static/js/runtime-main.02262636.js"
+  }
+]);
